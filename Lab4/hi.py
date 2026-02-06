d68b44d28cec6f04c2362084432f6f51d907429b
nprayoon <nprayoon@andrew.cmu.edu>
2026-02-06 12:55:29 -0500

    made the robot move

--- Lab4/hi.py
+++ Lab4/hi.py
@@ -1,38 +1,54 @@
 from motorgo import ControlMode
+from motorgo.plink import Plink
 import time
 
-left.control_mode = ControlMode.POWER
-right.control_mode = ControlMode.POWER
+LEFT_CH = 3
+RIGHT_CH = 1
 
-k_p = 15.0
-k_d = 0.8
+def main():
 
-prev_error = 0.0
-prev_t = time.time()
+    plink = Plink()
+    plink.connect()
+    imu = plink.imu
 
-while True:
-    # timing
-    t = time.time()
-    dt = t - prev_t
-    prev_t = t
-    if dt <= 0:
-        continue
+    left = getattr(plink, f"channel{LEFT_CH}")
+    right = getattr(plink, f"channel{RIGHT_CH}")
 
-    # sensor
-    g_theta = imu.gravity_vector[1]   # tilt estimate
+    left.control_mode = ControlMode.POWER
+    right.control_mode = ControlMode.POWER
 
-    # PD
-    error = -g_theta                  # target = 0
-    d_error = (error - prev_error) / dt
-    prev_error = error
+    k_p = 1.5
+    k_d = 2
 
-    u = k_p * error + k_d * d_error
+    prev_error = 0.0
+    prev_t = time.time()
 
-    # saturate
-    u = max(min(u, 1.0), -1.0)
+    while True:
+        # timing
+        t = time.time()
+        dt = t - prev_t
+        prev_t = t
+        if dt <= 0:
+            continue
 
-    # apply
-    left.power_command = u
-    right.power_command = u
+        # sensor
+        g_theta = imu.gravity_vector[1]   # tilt estimate
 
-    time.sleep(0.005)   # ~200 Hz
+        # PD
+        error = -g_theta                  # target = 0
+        d_error = (error - prev_error) / dt
+        prev_error = error
+
+        u = k_p * error + k_d * d_error
+
+        # saturate
+        u = max(min(u, 1.0), -1.0)
+
+        # apply
+        left.power_command = u
+        right.power_command = u
+
+        time.sleep(0.005)   # ~200 Hz
+
+if __name__ == "__main__":
+    main()
