4a633d80d5fab555b55c70cde4e935cbe7d0dca8
nprayoon <nprayoon@andrew.cmu.edu>
2026-02-22 18:14:25 -0500

    edited localization

--- lab6/localization.py
+++ lab6/localization.py
@@ -3,8 +3,14 @@ import board
 import adafruit_vl53l4cx
 from motorgo import Plink, ControlMode
 
+sector_num = 16
+map = [1,1,0,1,0,1,1,0,0,0,0,0,0,0,0,0]
+particles_num = 3200
+convergence_thresh = 0.7
+
+
 def main():
-    i2c = board.I2C()  # uses board.SCL and board.SDA
+    i2c = board.I2C()
 
     dist = adafruit_vl53l4cx.VL53L4CX(i2c)
 
@@ -15,6 +21,9 @@ def main():
     while True:
         dist.clear_interrupt()
         print("Distance: {} cm".format(dist.distance))
+
+if __name__ == "__main__":
+    main()
 
 
 
